7a7b1c9cf4d47c70a4835cb9db8a042c75bd4418
Bernard Tison <bernard.tison@gmail.com>
2025-04-15 11:53:38 +0200

    use quarkus quarkus-langchain4j-openai extension

--- src/main/java/org/parasol/customerservice/ai/structured/InformationExtractor.java
+++ src/main/java/org/parasol/customerservice/ai/structured/InformationExtractor.java
@@ -1,7 +1,11 @@
 package org.parasol.customerservice.ai.structured;
 
 import dev.langchain4j.service.SystemMessage;
+import io.quarkiverse.langchain4j.RegisterAiService;
+import jakarta.enterprise.context.ApplicationScoped;
 
+@RegisterAiService()
+@ApplicationScoped
 public interface InformationExtractor {
 
     @SystemMessage("Extract the customer support email information. Do not make up information. If a piece of information is not present, leave the field in the response blank")
--- src/main/java/org/parasol/customerservice/ai/structured/MessageProcessor.java
+++ src/main/java/org/parasol/customerservice/ai/structured/MessageProcessor.java
@@ -1,61 +1,20 @@
 package org.parasol.customerservice.ai.structured;
 
-import dev.langchain4j.model.chat.Capability;
-import dev.langchain4j.model.chat.ChatLanguageModel;
-import dev.langchain4j.model.openai.OpenAiChatModel;
-import dev.langchain4j.service.AiServices;
-import io.quarkus.runtime.StartupEvent;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.enterprise.event.Observes;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
+import jakarta.inject.Inject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Set;
 
 @ApplicationScoped
 public class MessageProcessor {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MessageProcessor.class);
 
-    @ConfigProperty(name = "langchain4j.openai.modelname")
-    String modelName;
-
-    @ConfigProperty(name = "langchain4j.openai.apikey")
-    String apiKey;
-
-    @ConfigProperty(name = "langchain4j.openai.baseurl")
-    String url;
-
-    ChatLanguageModel model;
-
+    @Inject
     InformationExtractor informationExtractor;
-
-    void onStart(@Observes StartupEvent e) {
-        model = initChatLanguageModel();
-        informationExtractor = initAiService();
-    }
 
     public StructuredInformation process(String message) {
         return informationExtractor.extractInformationFrom(message);
-    }
-
-    private ChatLanguageModel initChatLanguageModel() {
-        LOGGER.info("Initializing model");
-        return OpenAiChatModel.builder()
-                .apiKey(apiKey)
-                .modelName(modelName)
-                .baseUrl(url)
-                .logRequests(true)
-                .logResponses(true)
-                .supportedCapabilities(Set.of(Capability.RESPONSE_FORMAT_JSON_SCHEMA))
-                .strictJsonSchema(true)
-                .build();
-    }
-
-    private InformationExtractor initAiService() {
-        LOGGER.info("Initializing AiService");
-        return AiServices.create(InformationExtractor.class, model);
     }
 
 }
